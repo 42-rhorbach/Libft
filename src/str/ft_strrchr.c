0f702d7555549b0af62e7c77bc744776b0ebfa5e
QuiteHollow <robert123horbach@gmail.com>
2023-06-08 16:05:05 +0200

    improved strrchr

--- src/str/ft_strrchr.c
+++ src/str/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rhorbach <rhorbach@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/07 12:27:32 by rhorbach      #+#    #+#                 */
-/*   Updated: 2022/11/11 13:06:52 by rhorbach      ########   odam.nl         */
+/*   Updated: 2023/06/07 13:59:30 by rhorbach      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char		a;
 	char		*temp;
 	char		*str;
 	int			i;
 
 	temp = NULL;
 	str = (char *)s;
-	a = c;
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == a)
+		if (str[i] == c)
 			temp = &str[i];
 		i++;
 	}
-	if (a == '\0')
+	if (c == '\0')
 		temp = &str[i];
 	return (temp);
 }
